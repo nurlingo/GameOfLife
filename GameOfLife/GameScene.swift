e95ccd9f71510acea6981d5b5b73a7b615266c08
nurlingo <nursultan@outlook.com>
2017-06-27 11:13:34 -0700

    -game completed

--- GameOfLife/GameScene.swift
+++ GameOfLife/GameScene.swift
@@ -10,15 +10,56 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var gridNode: Grid!
+    var playButton: MSButtonNode!
+    var pauseButton: MSButtonNode!
+    var stepButton: MSButtonNode!
+    var populationLabel: SKLabelNode!
+    var generationLabel: SKLabelNode!
+    
     override func didMove(to view: SKView) {
         /* Setup your scene here */
+        
+        gridNode = self.childNode(withName: "gridNode") as! Grid
+        playButton = self.childNode(withName: "playButton") as! MSButtonNode
+        pauseButton = self.childNode(withName: "pauseButton") as! MSButtonNode
+        stepButton = self.childNode(withName: "stepButton") as! MSButtonNode
+        populationLabel = self.childNode(withName: "populationLabel") as! SKLabelNode
+        generationLabel = self.childNode(withName: "generationLabel") as! SKLabelNode
+        stepButton.selectedHandler = { self.stepSimulation() }
+        pauseButton.selectedHandler = { [unowned self] in
+            self.isPaused = true
+        }
+        playButton.selectedHandler = { [unowned self] in
+            self.isPaused = false
+        }
+
+        
+        let delay = SKAction.wait(forDuration: 0.5)
+        let callMethod = SKAction.perform(#selector(stepSimulation), onTarget: self)
+        let stepSequence = SKAction.sequence([delay,callMethod])
+        let simulation = SKAction.repeatForever(stepSequence)
+        self.run(simulation)
+        self.isPaused = true
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /* Called when a touch begins */
+        
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
+        populationLabel.text = String(gridNode.population)
+        generationLabel.text = String(gridNode.generation)
     }
+    
+    func stepSimulation() {
+        gridNode.evolve()
+        populationLabel.text = String(gridNode.population)
+        generationLabel.text = String(gridNode.generation)
+    }
+    
 }
--- GameOfLife/Grid.swift
+++ GameOfLife/Grid.swift
@@ -14,7 +14,7 @@ class Grid: SKSpriteNode {
     var gridArray = [[Creature]]()
     
     let rows = 8
-    let colums = 10
+    let columns = 10
     
     var cellWidth = 0
     var cellHeight = 0
@@ -27,9 +27,8 @@ class Grid: SKSpriteNode {
         let location = touch.location(in: self)
         let column = Int(location.x)/cellWidth
         let row = Int(location.y)/cellHeight
-        let creature = gridArray[column][row]
+        let creature = gridArray[row][column]
         creature.isAlive = !creature.isAlive
-        
         let populationAdded = creature.isAlive ? 1 : -1
         population += populationAdded
     }
@@ -37,29 +36,91 @@ class Grid: SKSpriteNode {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         isUserInteractionEnabled = true
-        cellWidth = Int(size.width)/colums
+        cellWidth = Int(size.width)/columns
         cellHeight = Int(size.height)/rows
         populateGrid()
     }
     
-    func addCreatureAtGrid(column: Int, row: Int) {
-        
+    func addCreatureAtGrid(row: Int, column: Int) {
         let creature = Creature()
         let position = CGPoint(x: cellWidth*column, y: cellHeight*row)
         creature.position = position
         creature.isAlive = false
         self.addChild(creature)
-        gridArray[column].append(creature)
-        
+        gridArray[row].append(creature)
     }
     
     func populateGrid() {
-        for column in 0..<colums {
+        for row in 0..<rows {
             gridArray.append([])
-            for row in 0..<rows {
-                addCreatureAtGrid(column: column, row: row)
+            for column in 0..<columns {
+                addCreatureAtGrid(row: row, column: column)
             }
         }
     }
     
+    func countNeighbourghs(){
+        
+        //loop through every creature in the grid
+        for column in 0..<columns {
+            for row in 0..<rows {
+                let creature = gridArray[row][column]
+                
+                //reset its neighborCount property to 0
+                creature.neighbourCount = 0
+                
+                //count neighbours
+                for cIndex in column-1...column+1 {
+                    
+                    if cIndex < 0 || cIndex == columns {continue}
+                    
+                    for rIndex in row-1...row+1 {
+                        
+                        if rIndex<0||rIndex == rows||(rIndex==row&&cIndex==column) {continue}
+                        
+                        let neighborCreature = gridArray[rIndex][cIndex]
+                        if neighborCreature.isAlive {
+                            creature.neighbourCount += 1
+                        }
+                    }
+                }
+                print(creature.neighbourCount)
+                
+            }
+            
+        }
+        
+    }
+    
+    func updateCreatures(){
+        
+        self.population = 0
+        
+        for column in 0..<columns {
+            for row in 0..<rows {
+                let creature = gridArray[row][column]
+                
+                switch creature.neighbourCount {
+                case 3:
+                    creature.isAlive = true
+                    break
+                case 0...1, 4...8:
+                    creature.isAlive = false
+                    break
+                default:
+                    //accounts for all other cases
+                    break
+                }
+                
+                if creature.isAlive {self.population += 1}
+            }
+            
+        }
+    }
+    
+    func evolve() {
+        countNeighbourghs()
+        updateCreatures()
+        generation+=1
+    }
 }
